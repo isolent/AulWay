095f874d8c66754c6ff1fc06e60c792de3317b9d
isolent <98767073+isolent@users.noreply.github.com>
2024-12-18 12:14:40 +0500

    first commit

---
+++ AulWayTests/AulWayTests.swift
@@ -0,0 +1,17 @@
+//
+//  AulWayTests.swift
+//  AulWayTests
+//
+//  Created by Aruzhan Kaharmanova on 14.12.2024.
+//
+
+import Testing
+@testable import AulWay
+
+struct AulWayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
